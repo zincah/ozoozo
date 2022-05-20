a69dc7da5195682e8ee0d66eb40440f252abf36c
zincah <leeah9737@naver.com>
2022-05-20 21:45:58 +0900

    브랜드페이지..

--- src/main/java/ozo/spring/house/user/controller/UserDealBestController.java
+++ src/main/java/ozo/spring/house/user/controller/UserDealBestController.java
@@ -4,13 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ozo.spring.house.seller.vo.SellerVO;
 import ozo.spring.house.user.dao.UserMainDAO;
 import ozo.spring.house.user.service.UserMainService;
 import ozo.spring.house.user.vo.UserCategoryVO;
 import ozo.spring.house.user.vo.UserProductVO;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UserDealBestController {
@@ -28,13 +34,34 @@ public class UserDealBestController {
 		return "ozotodaydeal_zinc";
 	}
 
+	// ºê·£µå
+	@RequestMapping(value = "/brandshop.com", method=RequestMethod.GET)
+	public String main_shop(Model model, UserProductVO vo, HttpServletRequest request){
+		
+		String brandcode = request.getParameter("brandcode");
+		vo.setPost_sellerid(Integer.parseInt(brandcode));
+		
+		if(request.getParameter("catecode") != null) {
+			String[] codes = request.getParameter("catecode").split("_");
+			vo.setPo_category(Integer.parseInt(codes[0]));
+			
+			if(codes.length == 2) {
+				System.out.println("subcategory click");
+				int subcate_code = Integer.parseInt(codes[1]);
+				vo.setInt(subcate_code/100*100); // mid
+				
+				if(subcate_code%100 != 0) {
+					vo.setPo_subcate(subcate_code); 
+				}
+			}
+			
+			List<List<UserCategoryVO>> sublist = userMainService.printCateList(vo);
+			model.addAttribute("sublist", sublist);
+			System.out.println("subli size" + sublist.size());
+		}
 
-	@RequestMapping(value = "/brandshop.com")
-	public String main_shop(Model model, UserProductVO vo){
-
-		List<UserProductVO> shopItemList = userMainService.shopItemList();
-		System.out.println(shopItemList.size());
-
+		
+		List<UserProductVO> shopItemList = userMainService.shopItemList(vo);
 
 		for(int i=0; i<shopItemList.size(); i++){
 			UserProductVO sho = shopItemList.get(i);
@@ -45,18 +72,29 @@ public class UserDealBestController {
 			sho.setSale_price(decFormat.format(sale_price));
 		}
 		
-		vo.setPost_sellerid(500001);
 		List<UserCategoryVO> toplist = userMainService.printTop(vo);
-		List<List<UserCategoryVO>> otherlist = userMainService.printCateList(vo);
+		SellerVO seller = userMainService.sellerInfo(vo);
+		
+		
+		List<UserCategoryVO> others = new ArrayList<UserCategoryVO>();
+		for(int i=0; i<toplist.size(); i++) {
+			UserCategoryVO cate = toplist.get(i);
+			
+			if(cate.getCate_code() == vo.getPo_category()) {
+				model.addAttribute("title", cate);
+			}else {
+				others.add(cate);
+			}
+		}
+		model.addAttribute("others", others);
+		
+		//List<List<UserCategoryVO>> otherlist = userMainService.printCateList(vo);
 
-		
-		System.out.println(toplist.size());
-		System.out.println(otherlist.size());
-		
-		model.addAttribute("toplist", toplist);
-		model.addAttribute("otherlist", otherlist);
+		//model.addAttribute("otherlist", otherlist);
+		model.addAttribute("seller", seller);
 		model.addAttribute("shopItemList", shopItemList);
-
+		model.addAttribute("toplist", toplist);
+		
 		return "ozoshop_main";
 	}
 }
--- src/main/java/ozo/spring/house/user/service/UserMainService.java
+++ src/main/java/ozo/spring/house/user/service/UserMainService.java
@@ -3,6 +3,7 @@ package ozo.spring.house.user.service;
 import java.util.List;
 
 import ozo.spring.house.admin.vo.BannerVO;
+import ozo.spring.house.seller.vo.SellerVO;
 import ozo.spring.house.user.vo.UserCategoryVO;
 import ozo.spring.house.user.vo.UserPagingVO;
 import ozo.spring.house.user.vo.UserProductVO;
@@ -19,9 +20,10 @@ public interface UserMainService {
 	public UserVO checkUserByNaver(UserVO vo); // ¿Å°Ü¾ßÇÔ
 	public void lastLoginCheck(UserVO vo); // ¿Å°Ü¾ßÇÔ
 	public List<UserProductVO> todayDealList();
-	public List<UserProductVO> shopItemList();
-	
+
 	// 
+	public List<UserProductVO> shopItemList(UserProductVO vo);
+	public SellerVO sellerInfo(UserProductVO vo);
 	public List<List<UserCategoryVO>> printCateList(UserProductVO vo);
 	public List<UserCategoryVO> printTop(UserProductVO vo);
 
